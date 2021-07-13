472b034e860151be1540fcb88bfa3fa85addfff7
santhosh4568 <santhoshpcse@gmail.com>
2021-07-13 12:43:33 +0530

    changed element ui to locale

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/en";
 
-Vue.config.productionTip = false
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
+Vue.config.productionTip = false;
+
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
